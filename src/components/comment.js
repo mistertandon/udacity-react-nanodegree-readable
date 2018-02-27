7fa9b6a4cc387fa2a2fb5306c2c6fa66040738c3
mistertandon <enggparveshtandon@gmail.com>
2018-02-27 14:26:41 +0530

    feat: Develop Comment component definition
    Develop render function definition for Comment component. Add Google fonts for
    comment body, action and info section.

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+import './../css/comment.css'
+
 import { getPostComments } from './../actions/commentAction'
 
 class Comment extends Component {
@@ -15,8 +17,30 @@ class Comment extends Component {
     const { comments } = this.props.comment;
 
     return (
-      <div>
-        Hello comments.js
+      <div className='comments--container'>
+        <div className='comment--header'>
+            Post comments
+        </div>
+        {
+          comments && comments.length && (
+            comments.map((comment, index) => (
+              <div>
+                <div className='comment--container'>
+                  <div className='comment--body'>
+                    <div className='comment--content'>{comment.body}</div>
+                    <div className='comment--actions'>
+                      Edit, Delete
+                    </div>
+                  </div>
+
+                  <div className='comment--info'>
+                    {`By ${comment.author} at ${new Date(comment.timestamp)}`}
+                  </div>
+                </div>
+              </div>
+            ))
+          )
+        }
       </div>
     )
   }
