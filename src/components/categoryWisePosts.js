63f3cc1072129a4c4ab203fae7d29476d53a1f88
mistertandon <enggparveshtandon@gmail.com>
2018-03-06 15:48:28 +0530

    refactor: Refactor categoryWiseComponent and App component

--- src/components/categoryWisePosts.js
+++ src/components/categoryWisePosts.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Route, Link } from 'react-router-dom'
 
 import './../css/categoryWise.css'
 
-import { addCategories } from './../actions/categoryAction'
 import { addPosts } from './../actions/postAction'
 import Post from './post'
 import PostGridLink from './postGridLink'
 
 class CategoryWisePosts extends Component {
 
-
   render() {
 
     const { activeCategory } = this.props.location.state;
-    const posts = [];
+
     return (
       <div className='category--page'>
         <div className='category--title'>
@@ -32,5 +28,4 @@ class CategoryWisePosts extends Component {
   }
 }
 
-// export default connect(mapStateToProps, mapDispatchToProps)(CategoryWisePosts)
 export default CategoryWisePosts;
